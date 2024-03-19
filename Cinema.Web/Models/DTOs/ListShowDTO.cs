db347e0da0e0e4993ec81c2ab3a3736d4ed7027a
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-03-19 17:25:12 +0100

    can't reserve seat if there are no seats left

--- Cinema.Web/Models/DTOs/ListShowDTO.cs
+++ Cinema.Web/Models/DTOs/ListShowDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Cinema.Web.Models.Tables;
+using Cinema.Web.Models.Tables.Enums;
 using System.Linq.Expressions;
 
 namespace Cinema.Web.Models.DTOs
@@ -8,13 +9,15 @@ namespace Cinema.Web.Models.DTOs
         public Int32 Id { get; set; }
         public String Title { get; set; } = null!;
         public DateTime Start { get; set; }
+        public Int32 FreeSeatCount { get; set; }
 
         public static Expression<Func<Show, ListShowDTO>> Projection { get; }
             = show => new ListShowDTO()
             {
                 Id = show.Id,
                 Title = show.Movie.Title,
-                Start = show.Start
+                Start = show.Start,
+                FreeSeatCount = show.Seats.Count(s => s.Status == Status.Free)
             };
 
         public static ListShowDTO Create(Show show) => Projection.Compile().Invoke(show);
